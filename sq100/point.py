7600864720398220ec61427837ab8423ed6b390c
Timo Nachstedt <mail@nachstedt.com>
2017-01-26 23:19:18 +0100

    continued implementing reading of trackpoint data

--- sq100/point.py
+++ sq100/point.py
@@ -1,18 +1,4 @@
 class Point(object):
-    def __init__(self, latitude = 0, longitude = 0):
-        self.latitude  = Coordinate(latitude)
-        self.longitude = Coordinate(longitude)
-        
-    def __getitem__(self, attr):
-        return getattr(self, attr)
-        
-    def __hex__(self):
-        return '%s%s' % (hex(self.latitude), hex(self.longitude))
-    
-    def fromHex(self, hex):
-        if len(hex) == 16:
-            self.latitude = Coordinate().fromHex(hex[:8])
-            self.longitude = Coordinate().fromHex(hex[8:])
-            return self
-        else:
-            raise GH600ParseException(self.__class__.__name__, len(hex), 16)
+    def __init__(self, latitude = None, longitude = None):
+        self.latitude  = latitude
+        self.longitude = longitude
