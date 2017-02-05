ab9d1d03ebb9fdf269f96b8f745cdaea8726e828
Timo Nachstedt <mail@nachstedt.com>
2017-02-05 13:27:16 +0100

    added test for ArivalSQ100::_unpack_track_info_parameter

--- sq100/test/test_arival_sq100.py
+++ sq100/test/test_arival_sq100.py
@@ -1,3 +1,4 @@
+import datetime
 import mock
 import struct
 
@@ -31,3 +32,50 @@ def test_unpack_message():
     assert(data.parameter == parameter)
     assert(data.payload_length == payload_length)
     assert(data.checksum == checksum)
+
+
+def test_unpack_track_info_parameter():
+    date = datetime.datetime(2016, 7, 23, 14, 30, 11)
+    no_track_points = 1230
+    duration = datetime.timedelta(seconds=2345)
+    distance = 4321
+    no_laps = 3
+    memory_block_index = 51
+    track_id = 13
+    calories = 714
+    max_speed = 89
+    max_heart_rate = 198
+    avg_heart_rate = 153
+    asc_height = 873
+    des_height = 543
+    min_height = 345
+    max_height = 1122
+
+    parameter = struct.pack(
+        ">6B3I5HB3H2B4H13s",
+        date.year - 2000, date.month, date.day,
+        date.hour, date.minute, date.second,
+        no_track_points, duration.seconds * 10, distance,
+        no_laps, 0, memory_block_index, 0, track_id,
+        0,
+        calories, 0, max_speed,
+        max_heart_rate, avg_heart_rate,
+        asc_height, des_height, min_height, max_height,
+        b'')
+
+    track = ArivalSQ100._unpack_track_info_parameter(parameter)
+    assert(track.date == date)
+    assert(track.no_track_points == no_track_points)
+    assert(track.duration == duration)
+    assert(track.distance == distance)
+    assert(track.no_laps == no_laps)
+    assert(track.memory_block_index == memory_block_index)
+    assert(track.id == track_id)
+    assert(track.calories == calories)
+    assert(track.max_speed == max_speed)
+    assert(track.max_heart_rate == max_heart_rate)
+    assert(track.avg_heart_rate == avg_heart_rate)
+    assert(track.ascending_height == asc_height)
+    assert(track.descending_height == des_height)
+    assert(track.min_height == min_height)
+    assert(track.max_height == max_height)
--- sq100/track.py
+++ sq100/track.py
@@ -9,14 +9,14 @@ class Track(object):
                  descending_height=None,
                  distance=None,
                  duration=None,
-                 lap_count=None,
                  max_heart_rate=None,
                  max_height=None,
                  max_speed=None,
                  memory_block_index=None,
                  min_height=None,
+                 no_laps=None,
+                 no_track_points=None,
                  track_id=None,
-                 trackpoint_count=None,
                  ):
         self.ascending_height = ascending_height
         self.avg_heart_rate = avg_heart_rate
@@ -26,11 +26,11 @@ class Track(object):
         self.distance = distance
         self.duration = duration
         self.id = track_id
-        self.lap_count = lap_count
         self.max_heart_rate = max_heart_rate
         self.max_height = max_height
         self.max_speed = max_speed
         self.memory_block_index = memory_block_index
         self.min_height = min_height
-        self.trackpoint_count = trackpoint_count
+        self.no_laps = no_laps
+        self.no_track_points = no_track_points
         self.trackpoints = []
