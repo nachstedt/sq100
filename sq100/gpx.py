01ba8193d4ec9f4f4ab2c0a6ceaecdc15e6f35f4
Timo Nachstedt <mail@nachstedt.com>
2017-02-25 14:50:57 +0100

    renamed gpx methods

--- sq100/gpx.py
+++ sq100/gpx.py
@@ -10,7 +10,8 @@ tpex_ns = 'http://www.garmin.com/xmlschemas/TrackPointExtension/v2'
 tpex_ns_def = "https://www8.garmin.com/xmlschemas/TrackPointExtensionv2.xsd"
 
 
-def _bounds_element(min_latitude, max_latitude, min_longitude, max_longitude):
+def _create_bounds_element(min_latitude, max_latitude, min_longitude,
+                           max_longitude):
     bounds = etree.Element(etree.QName(gpx_ns, "bounds"))
     bounds.set("minlat", str(min_latitude))
     bounds.set("minlon", str(min_longitude))
@@ -19,87 +20,89 @@ def _bounds_element(min_latitude, max_latitude, min_longitude, max_longitude):
     return bounds
 
 
-def _datetime_element(namespace, name, value):
+def _create_datetime_element(namespace, name, value):
     if value.tzinfo is not None:
         value = (value - value.utcoffset()).replace(tzinfo=None)
-    return _string_element(namespace, name, "%sZ" % value.isoformat())
+    return _create_string_element(namespace, name, "%sZ" % value.isoformat())
 
 
-def _decimal_element(namespace, name, value):
-    return _string_element(namespace, name, "%d" % value)
+def _create_decimal_element(namespace, name, value):
+    return _create_string_element(namespace, name, "%d" % value)
 
 
-def _garmin_track_point_extension_element(track_point):
+def _create_garmin_track_point_extension_element(track_point):
     trkptex = etree.Element(etree.QName(tpex_ns, "TrackPointExtension"))
-    trkptex.append(_decimal_element(tpex_ns, "hr", track_point.heart_rate))
+    trkptex.append(
+        _create_decimal_element(tpex_ns, "hr", track_point.heart_rate))
     return trkptex
 
 
-def _gpx_element(tracks):
+def _create_gpx_element(tracks):
     nsmap = {None: gpx_ns, 'xsi': xsi_ns, 'garmin': tpex_ns}
     gpx = etree.Element('gpx', nsmap=nsmap)
     gpx.set('version', '1.1')
     gpx.set("creator", 'https://github.com/tnachstedt/sq100')
     gpx.set(etree.QName(xsi_ns, "schemaLocation"),
             "%s %s %s %s" % (gpx_ns, gpx_ns_def, tpex_ns, tpex_ns_def))
-    gpx.append(_metadata_element())
+    gpx.append(_create_metadata_element())
     for i, track in enumerate(tracks):
-        gpx.append(_track_element(track, i))
+        gpx.append(_create_track_element(track, i))
     return gpx
 
 
-def _metadata_element():
+def _create_metadata_element():
     metadata = etree.Element(etree.QName(gpx_ns, 'metadata'))
-    metadata.append(_string_element(gpx_ns, "name", "my name"))
-    metadata.append(_string_element(gpx_ns, "desc", "my_description"))
+    metadata.append(_create_string_element(gpx_ns, "name", "my name"))
+    metadata.append(_create_string_element(gpx_ns, "desc", "my_description"))
     metadata.append(
-        _datetime_element(gpx_ns, "time", datetime.datetime.now()))
-    metadata.append(_bounds_element())
+        _create_datetime_element(gpx_ns, "time", datetime.datetime.now()))
+    metadata.append(_create_bounds_element())
     return metadata
 
 
-def _string_element(namespace, name, value):
+def _create_string_element(namespace, name, value):
     elem = etree.Element(etree.QName(namespace, name))
     elem.text = value
     return elem
 
 
-def _track_element(track, number):
+def _create_track_element(track, number):
     trk = etree.Element(etree.QName(gpx_ns, 'trk'))
-    trk.append(_string_element(gpx_ns, "name", "GPS track name"))
-    trk.append(_string_element(gpx_ns, "cmt", track.id))
-    trk.append(_string_element(gpx_ns, "desc", "user description"))
-    trk.append(_string_element(gpx_ns, "src", "Arival SQ100 computer"))
-    trk.append(_string_element(gpx_ns, "number", number))
-    trk.append(_track_segment_element(track))
+    trk.append(_create_string_element(gpx_ns, "name", "GPS track name"))
+    trk.append(_create_string_element(gpx_ns, "cmt", track.id))
+    trk.append(_create_string_element(gpx_ns, "desc", "user description"))
+    trk.append(_create_string_element(gpx_ns, "src", "Arival SQ100 computer"))
+    trk.append(_create_string_element(gpx_ns, "number", number))
+    trk.append(_create_track_segment_element(track))
     return trk
 
 
-def _track_point_element(track_point):
+def _create_track_point_element(track_point):
     trkpt = etree.Element(etree.QName(gpx_ns, "trkpt"))
     trkpt.set("lat", track_point.latitude)
     trkpt.set("lon", track_point.longitude)
-    trkpt.append(_decimal_element(gpx_ns, "ele", track_point.altitude))
-    trkpt.append(_datetime_element(gpx_ns, "time", track_point.time))
-    trkpt.append(_track_point_extensions_element(track_point))
+    trkpt.append(_create_decimal_element(gpx_ns, "ele", track_point.altitude))
+    trkpt.append(_create_datetime_element(gpx_ns, "time", track_point.time))
+    trkpt.append(_create_track_point_extensions_element(track_point))
     return trkpt
 
 
-def _track_point_extensions_element(track_point):
+def _create_track_point_extensions_element(track_point):
     extensions = etree.Element(etree.QName(gpx_ns, "extensions"))
-    extensions.append(_garmin_track_point_extension_element(track_point))
+    extensions.append(
+        _create_garmin_track_point_extension_element(track_point))
     return extensions
 
 
-def _track_segment_element(track):
+def _create_track_segment_element(track):
     segment = etree.Element(etree.QName(gpx_ns, "trkseg"))
     for track_point in track.track_points:
-        segment.append(_track_point_element(track_point))
+        segment.append(_create_track_point_element(track_point))
     return segment
 
 
 def tracks_to_gpx(tracks, filename):
-    gpx = _gpx_element(tracks)
+    gpx = _create_gpx_element(tracks)
     doc = etree.ElementTree(gpx)
     doc.write(filename,
               encoding="UTF-8",
--- sq100/test/test_gpx.py
+++ sq100/test/test_gpx.py
@@ -6,12 +6,12 @@ from lxml import etree
 import sq100.gpx as gpx
 
 
-def test_bounds_element():
+def test_create_bounds_element():
     min_latitude = 10.8
     max_latitude = 25.0
     min_longitude = -8.1
     max_longitude = -2.9
-    bounds = gpx._bounds_element(
+    bounds = gpx._create_bounds_element(
         min_latitude=min_latitude, max_latitude=max_latitude,
         min_longitude=min_longitude, max_longitude=max_longitude)
     assert float(bounds.get("minlat")) == min_latitude
@@ -20,34 +20,37 @@ def test_bounds_element():
     assert float(bounds.get("maxlon")) == max_longitude
 
 
-def test_datetime_element_with_tzinfo():
+def test_create_datetime_element_with_tzinfo():
     dt = datetime.datetime(
         year=1987, month=12, day=19, hour=15, minute=30, second=20,
         tzinfo=datetime.timezone(datetime.timedelta(hours=1)))
-    elem = gpx._datetime_element(namespace="timo", name="birth", value=dt)
+    elem = gpx._create_datetime_element(
+        namespace="timo", name="birth", value=dt)
     assert elem.tag == "{timo}birth"
     assert elem.text == "1987-12-19T14:30:20Z"
 
 
-def test_datetime_element_without_tzinfo():
+def test_create_datetime_element_without_tzinfo():
     dt = datetime.datetime(
         year=1987, month=12, day=19, hour=15, minute=30, second=20)
-    elem = gpx._datetime_element(namespace="timo", name="birth", value=dt)
+    elem = gpx._create_datetime_element(
+        namespace="timo", name="birth", value=dt)
     assert elem.tag == "{timo}birth"
     assert elem.text == "1987-12-19T15:30:20Z"
 
 
-def test_decimal_element():
-    elem = gpx._decimal_element(namespace="timo", name="number", value=-42)
+def test_create_decimal_element():
+    elem = gpx._create_decimal_element(
+        namespace="timo", name="number", value=-42)
     assert elem.tag == '{timo}number'
     assert int(elem.text) == -42
 
 
-def test_garmin_track_point_extension_element():
+def test_create_garmin_track_point_extension_element():
     track_point = MagicMock()
     track_point.heart_rate = 150
     ns = 'http://www.garmin.com/xmlschemas/TrackPointExtension/v2'
-    elem = gpx._garmin_track_point_extension_element(track_point)
+    elem = gpx._create_garmin_track_point_extension_element(track_point)
     assert elem.tag == ("{%s}TrackPointExtension" % ns)
     print(etree.tostring(elem))
     assert int(elem.find('{%s}hr' % ns).text) == 150
