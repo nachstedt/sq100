b964924657eda261d8329061d814d8277e3041a3
Timo Nachstedt <mail@nachstedt.com>
2017-02-26 22:59:07 +0100

    added GNU GPL v3 license

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
+# SQ100 - Serial Communication with the a-rival SQ100 heart rate computer
+# Copyright (C) 2017  Timo Nachstedt
+#
+# This file is part of SQ100.
+#
+# SQ100 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SQ100 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import print_function
-from setuptools import setup, find_packages
-import io
-import codecs
-import os
-import sys
+from setuptools import setup
 
 
 setup(
     name='sq100',
-    version="0.0.1",
+    version="0.1.0",
     url='http://github.com/tnachstedt/sq100/',
     license='Apache Software License',
     author='Timo Nachstedt',
